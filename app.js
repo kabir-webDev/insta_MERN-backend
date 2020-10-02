e1178c77dd00976808d8677c2aef94a3a1396987
kabir-webDev <kabir.webdev@gmail.com>
2020-10-02 18:35:14 +0600

    User Checking

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const PORT = 5000;
 const { MONGOURI } = require("./keys");
 
 require("./models/user");
+app.use(express.json());
+
+app.use(require("./routes/auth"));
 
 mongoose.connect(MONGOURI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
